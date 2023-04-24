f52681e2e6a271d3830df6318e15b39d2b2b665a
Abigail Flynn <120720780+abiflynn@users.noreply.github.com>
2023-04-24 11:50:49 +0100

    update skincrae

--- .streamlit/pages/2. Skincare.py
+++ .streamlit/pages/2. Skincare.py
@@ -5,7 +5,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import euclidean_distances
 import time 
 
-st.image(".streamlit/streamlit_images/Screenshot 2023-04-10 at 18.27.54.png")
+st.image(".streamlit/streamlit_images/Screenshot 2023-04-12 at 00.52.29.png")
 
 skincare_clusters = pd.read_csv("./exported_csvs/skincare_clusters.csv")
 

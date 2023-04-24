3d2f22cc8eb8486a7d14495f4f1b2c432e4fb277
Abigail Flynn <120720780+abiflynn@users.noreply.github.com>
2023-04-24 11:54:15 +0100

    update constraints

--- .streamlit/pages/4. Constraints.py
+++ .streamlit/pages/4. Constraints.py
@@ -6,7 +6,7 @@ from sklearn.metrics.pairwise import euclidean_distances
 import time 
 from sklearn.preprocessing import MinMaxScaler
 
-st.image('/Users/abigailflynn/Desktop/WBS CODING /Final Project /streamlit_images/Screenshot 2023-04-11 at 17.12.19.png')
+st.image('.streamlit/streamlit_images/Screenshot 2023-04-11 at 17.12.19.png')
 
 st.write("""As with any recommendation system the lack of comprehensive data was a major limitation of this project. Without a thorough understanding of user preferences, behaviors, and demographics, the system is unable to make accurate and personalised recommendations. Additionally, using a limited dataset can also limit the input options available for users, which can negatively impact the customer experience.
 """)
@@ -31,21 +31,21 @@ with col1:
 with col2:
     st.title("")
     st.title("")
-    st.image("/Users/abigailflynn/Desktop/WBS CODING /Final Project /streamlit_images/Screenshot 2023-04-11 at 21.13.52.png")
+    st.image(".streamlit/streamlit_images/Screenshot 2023-04-11 at 21.13.52.png")
 
 st.subheader('Future Capabilities:')
 st.write("""If these limitations were not a factor, the project could evolve to include data on user demographics, behaviours, preferences, and past interactions, allowing for a more personalised and effective approach to recommendations. As well as this with access to sales and market data this interface would be able to include recommendations based on sales, new releases and market trends. With a bigger dataset including more brands and products the blue sky future of BeautyBOT is to become the Netflix of Beauty.
 """)
 
-st.image("/Users/abigailflynn/Desktop/WBS CODING /Final Project /streamlit_images/Screenshot 2023-04-11 at 22.37.36.png")
-st.image("/Users/abigailflynn/Desktop/WBS CODING /Final Project /streamlit_images/Screenshot 2023-04-10 at 17.40.31.png")
-st.image("/Users/abigailflynn/Desktop/WBS CODING /Final Project /streamlit_images/Screenshot 2023-04-10 at 17.39.47.png")
+st.image(".streamlit/streamlit_images/Screenshot 2023-04-11 at 22.37.36.png")
+st.image(".streamlit/streamlit_images/Screenshot 2023-04-10 at 17.40.31.png")
+st.image(".streamlit/streamlit_images/Screenshot 2023-04-10 at 17.39.47.png")
 
 # col3, col4 = st.columns(2)
 
 # with col3:
 #     st.title("")
-#     st.image("/Users/abigailflynn/Desktop/WBS CODING /Final Project /streamlit_images/Screenshot 2023-04-11 at 21.13.52.png")
+#     st.image(".streamlit/streamlit_images/Screenshot 2023-04-11 at 21.13.52.png")
 
 # with col4:
 #     st.title("")

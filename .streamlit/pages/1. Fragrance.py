959a51c7d8decdf2202769afc52c2356db31ac80
Abigail Flynn <120720780+abiflynn@users.noreply.github.com>
2023-04-24 11:32:39 +0100

    update frag

--- .streamlit/pages/1. Fragrance.py
+++ .streamlit/pages/1. Fragrance.py
@@ -18,11 +18,11 @@ st.write(""":brown_heart: The top fragrances based on user ratings.""")
 
 
 #read in the dataset
-perfume_merge_women = pd.read_csv("./exported_csvs/perfume_merge_women.csv")
-perfume_clusters_women = pd.read_csv("./exported_csvs/perfume_clusters_women.csv")
+perfume_merge_women = pd.read_csv("./exported_csvs/perfume_merge_women_1.csv")
+perfume_clusters_women = pd.read_csv("./exported_csvs/perfume_clusters_women_1.csv")
 
-perfume_merge_men = pd.read_csv("./exported_csvs/perfume_merge_men.csv")
-perfume_clusters_men = pd.read_csv("./exported_csvs/perfume_clusters_men.csv")
+perfume_merge_men = pd.read_csv("./exported_csvs/perfume_merge_men_1.csv")
+perfume_clusters_men = pd.read_csv("./exported_csvs/perfume_clusters_men_1.csv")
 
 if gender == "Female":
     top_perfumes = perfume_clusters_women.sort_values(by='item_rating', ascending=False)[["brand", "name"]].head(5)
